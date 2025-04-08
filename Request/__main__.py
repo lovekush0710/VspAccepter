4bfbc8b3d4865569e3e6234ede90bdcc2f38d0ff
lovekush0710 <ksjayhind@gmail.com>
2025-04-08 16:39:10 +0530

    Add files via upload

---
+++ Request/__main__.py
@@ -0,0 +1,27 @@
+import asyncio
+import importlib
+from config import START_IMG,OWNER_ID
+from pyrogram import idle
+
+from Request import LOGGER, Mukesh
+from Request.modules import ALL_MODULES
+
+
+async def _start():
+    try:
+        await Mukesh.start()
+    except Exception as ex:
+        LOGGER.error(ex)
+        quit(1)
+
+    for all_module in ALL_MODULES:
+        importlib.import_module("Request.modules." + all_module)
+
+    LOGGER.info(f"@{Mukesh.username} Started.")
+    await Mukesh.send_photo(6748451207,START_IMG,"❤️I am Alive boss❤️")
+    await idle()
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(_start())
+    LOGGER.info("Stopping  bot")
