2f5d5d3097e1b9aa24bd7dadeed17050cd242d18
lovekush0710 <ksjayhind@gmail.com>
2026-03-06 19:30:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
-API_ID = os.environ.get("API_ID", "27072814")
-API_HASH = os.environ.get("API_HASH", "68e0b4f357cd483d4b891ac6bf0e7e6a")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7228121605:AAGM_p3DlpjyaCo89cdvwy32-B9YgvbgIak")
+API_ID = os.environ.get("API_ID", "31046740")
+API_HASH = os.environ.get("API_HASH", "a164b63a4511be9072b0af43bda57249")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8100770013:AAF2lx5Kb5kDxsH1wDKCAllh3B4IwrSXZa0")
 START_IMG = os.environ.get(
     "START_IMG", "https://graph.org/file/7c238bd4dea9733613eb8.jpg"
 )
 
 APPROVED = os.environ.get("APPROVED", "on").lower()
-OWNER_ID=os.environ.get("OWNER_ID", "6748451207")
-ALL_CHAT=[-1002220717270, -1002234602475, -1002215220596, -1002160342193, -1002223971836, -1002175682830, -1002212816386, -1002235408353]
-MAIN_CHAT_ID=os.environ.get("MAIN_CHAT_ID", -1002244183546)
+OWNER_ID=os.environ.get("OWNER_ID", "8279029904")
+ALL_CHAT=[-1003788989352, -1003792007602, -1002215220596]
+MAIN_CHAT_ID=os.environ.get("MAIN_CHAT_ID", -1003855822374)
