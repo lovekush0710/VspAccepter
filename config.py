793830ab5dfd82f207bcb40616434cfb5656ac6a
lovekush0710 <ksjayhind@gmail.com>
2026-04-21 13:56:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = os.environ.get("API_ID", "31046740")
 API_HASH = os.environ.get("API_HASH", "a164b63a4511be9072b0af43bda57249")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8375341328:AAFQOGxWMbMh3YPmJRsQR_22F1fH4R132Co")
 START_IMG = os.environ.get(
-    "START_IMG", "https://graph.org/file/7c238bd4dea9733613eb8.jpg"
+    "START_IMG", "https://i.ibb.co/MkTPDc5P/x.jpg"
 )
 
 APPROVED = os.environ.get("APPROVED", "on").lower()
