d77530d2925b4cb33ad3fda60bf9e708a2997af4
lovekush0710 <ksjayhind@gmail.com>
2026-04-21 13:07:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 API_ID = os.environ.get("API_ID", "31046740")
 API_HASH = os.environ.get("API_HASH", "a164b63a4511be9072b0af43bda57249")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8100770013:AAF2lx5Kb5kDxsH1wDKCAllh3B4IwrSXZa0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8375341328:AAFQOGxWMbMh3YPmJRsQR_22F1fH4R132Co")
 START_IMG = os.environ.get(
     "START_IMG", "https://graph.org/file/7c238bd4dea9733613eb8.jpg"
 )
 
 APPROVED = os.environ.get("APPROVED", "on").lower()
 OWNER_ID=os.environ.get("OWNER_ID", "8279029904")
-ALL_CHAT=[-1003788989352, -1003792007602]
-MAIN_CHAT_ID=os.environ.get("MAIN_CHAT_ID", -1003855822374)
+ALL_CHAT=[-1003788989352, -1003792007602, -1003208690267]
+MAIN_CHAT_ID=os.environ.get("MAIN_CHAT_ID", -1003829919384)
